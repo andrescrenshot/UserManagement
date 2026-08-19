c0950158e332ce931ba1dc271bfac5152c063390
andrescrenshot <gaharugabut@gmail.com>
2026-08-19 19:43:04 +0700

    Fix CORS for Vercel frontend

--- Beuser/util/cors_middleware.py
+++ Beuser/util/cors_middleware.py
@@ -2,37 +2,72 @@ import falcon
 
 
 class CORSMiddleware:
+
     def process_request(self, req, resp):
+
         origin = req.get_header("Origin")
 
-        if origin == "http://localhost:5173":
-            resp.set_header("Access-Control-Allow-Origin", origin)
+        allowed_origins = [
+            "http://localhost:5173",
+            "https://usermanagement-gaharu.vercel.app",
+        ]
 
-        resp.set_header("Access-Control-Allow-Credentials", "true")
+        if origin in allowed_origins:
+            resp.set_header(
+                "Access-Control-Allow-Origin",
+                origin
+            )
+
+        resp.set_header(
+            "Access-Control-Allow-Credentials",
+            "true"
+        )
+
         resp.set_header(
             "Access-Control-Allow-Headers",
             "Content-Type, Authorization"
         )
+
         resp.set_header(
             "Access-Control-Allow-Methods",
             "GET, POST, PUT, DELETE, OPTIONS"
         )
 
-        # WAJIB: tangani preflight request
         if req.method == "OPTIONS":
-            raise falcon.HTTPStatus(falcon.HTTP_204)
+            resp.status = falcon.HTTP_204
 
-    def process_response(self, req, resp, resource, req_succeeded):
+
+    def process_response(
+        self,
+        req,
+        resp,
+        resource,
+        req_succeeded
+    ):
+
         origin = req.get_header("Origin")
 
-        if origin == "http://localhost:5173":
-            resp.set_header("Access-Control-Allow-Origin", origin)
+        allowed_origins = [
+            "http://localhost:5173",
+            "https://usermanagement-gaharu.vercel.app",
+        ]
 
-        resp.set_header("Access-Control-Allow-Credentials", "true")
+        if origin in allowed_origins:
+            resp.set_header(
+                "Access-Control-Allow-Origin",
+                origin
+            )
+
+        resp.set_header(
+            "Access-Control-Allow-Credentials",
+            "true"
+        )
+
         resp.set_header(
             "Access-Control-Allow-Headers",
             "Content-Type, Authorization"
         )
+
         resp.set_header(
             "Access-Control-Allow-Methods",
             "GET, POST, PUT, DELETE, OPTIONS"
