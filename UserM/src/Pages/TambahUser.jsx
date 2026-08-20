f1cb4743a3882b1bf2ff2ea8270af150257cfc32
andrescrenshot <gaharugabut@gmail.com>
2026-08-20 15:41:58 +0700

    Perbaikan5

--- UserM/src/Pages/TambahUser.jsx
+++ UserM/src/Pages/TambahUser.jsx
@@ -254,18 +254,6 @@ export default function TambahUser() {
     >
       <div
         style={{
-          background: "#101B4C",
-          color: "white",
-          padding: "16px 28px",
-          fontSize: "16px",
-          fontWeight: 600,
-        }}
-      >
-        CRM For Education Binus
-      </div>
-
-      <div
-        style={{
           flex: 1,
           display: "flex",
           justifyContent: "center",
